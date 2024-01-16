003e34af130c79949ae56ee7013f74311651ecbe
Indominuz18 <manmitsingh2003@gmail.com>
2024-01-15 23:36:12 -0800

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Manmeet Singh on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
